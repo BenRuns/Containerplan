f3e602fd478a27607acb834c7b1d2c4c39965e8c
Ben <BenRuns@users.noreply.github.com>
2013-10-02 13:51:14 -0700

    Update CubicMeasureCalc.py

--- CubicMeasureCalc.py
+++ CubicMeasureCalc.py
@@ -13,6 +13,20 @@ def WhichContainer(size):# returns the containers that can contain the cubic met
 		return None
 	else:
 		return CNTS
+def isTooLong(length,Container):#if the length won't fit into container, returns FALSE
+
+	if length > Container['Length']:
+		return False
+	else:
+		return True
+
+def isTooHigh(Height,Container):#if the Height won't fit into container, returns FALSE
+
+	if Height > Container['Height']:
+		return False
+	else:
+		return True
+		
 
 FortyFootGP = {'Name':"40' Standard" ,'Length':473, 'Width':92, 'Height':94,'DoorWidth':92,'DoorHeight':90,'Capacity':76.28} #inside and outside dimension in inches for 40' cnt
 FortyFootHC = {'Name':"40' High Cube",'Length':473, 'Width':92, 'Height':107,'DoorWidth':92,'DoorHeight':101,'Capacity':76.28} #inside and outside dimension in inches for 40' high cube
